e0af2bd0a5a8e76eaf2f81e320876c73703b4665
Max <maxtclinton@gmail.com>
2025-12-30 15:09:41 -0500

    add beamchain_addrman module

--- src/beamchain_node_sup.erl
+++ src/beamchain_node_sup.erl
@@ -19,6 +19,7 @@ init([]) ->
         child_spec(beamchain_db, worker),
         child_spec(beamchain_chainstate, worker),
         child_spec(beamchain_mempool, worker),
+        child_spec(beamchain_addrman, worker),
         child_spec(beamchain_peer_manager, worker),
         child_spec(beamchain_sync, worker)
         %% beamchain_rpc will be added when cowboy is wired up
